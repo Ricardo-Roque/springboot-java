8e945c981585d54b9371c315a5ab963625b21be6
ricardo.roque <ricardo.roque@neomind.com.br>
2024-10-12 12:49:57 -0300

    Service layer, component registration

--- src/main/java/org/example/course/config/TestConfig.java
+++ src/main/java/org/example/course/config/TestConfig.java
@@ -1,13 +1,10 @@
 package org.example.course.config;
 
-import org.example.course.entities.User;
 import org.example.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-
-import java.util.Arrays;
 
 @Configuration
 @Profile("default")
@@ -18,10 +15,5 @@ public class TestConfig implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-
-        User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
-        User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
-
-        userRepository.saveAll(Arrays.asList(u1, u2));
     }
 }
--- src/main/java/org/example/course/controller/UserController.java
+++ src/main/java/org/example/course/controller/UserController.java
@@ -1,18 +1,32 @@
 package org.example.course.controller;
 
 import org.example.course.entities.User;
+import org.example.course.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/findAll")
-    public ResponseEntity<User> findAll() {
-        User user = new User(1L, "Ricardo Roque", "ricardo@gmail.com", "999999999", "123456");
-        return ResponseEntity.ok().body(user);
+    public ResponseEntity<List<User>> findAll() {
+        List<User> userList = userService.findAll();
+        return ResponseEntity.ok(userList);
+    }
+
+    @GetMapping("findById/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User user = userService.findById(id);
+        return ResponseEntity.ok(user);
     }
 }
